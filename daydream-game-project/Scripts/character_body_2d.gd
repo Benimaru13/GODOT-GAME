cf551ff5504b557b621fcc6e96463347090a7723
Chibueze <benprichi13@gmail.com>
2025-09-27 14:33:23 -0700

    player has animations now
    i spent forever working on the animations

---
+++ daydream-game-project/Scripts/character_body_2d.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
+const falling = true
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		animated_sprite.play("fall")
+
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_up") and velocity.y < 0:
+		velocity.y = JUMP_VELOCITY / 4
+	
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+
+	if direction:
+		velocity.x = direction * SPEED
+		animated_sprite.play("run")
+		animated_sprite.flip_h = direction < 0
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite.play("idle")
+
+	move_and_slide()
