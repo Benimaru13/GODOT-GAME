84e6902f9d7ca387fb3669eabb1ef3286343d354
srija-belide <srija.belide@gmail.com>
2025-09-27 17:44:25 -0700

    background

--- daydream-game-project/Srija's changes/Srija Scenes/area_2d.gd
+++ daydream-game-project/Srija's changes/Srija Scenes/area_2d.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-
+@export var next_scene: PackedScene
 # Called when the node enters the scene tree for the first time.
 
 
-func _on_body_entered(body: Node2D) -> void:
-	print("Lava Entered")
+func _on_body_entered(body: Node) -> void:
+	if body.name == "Player":
+		get_tree().change_scene_to_file(next_scene.resource_path)
+	
